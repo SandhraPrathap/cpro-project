21d3da31869073a2481e5aa60d7cece3085eb698
SandhraPrathap <sandhraprathap@gmail.com>
2020-06-13 22:26:17 +0530

    lsearch

---
+++ lsearch.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[20],m,j=0;
+    printf("Enter the no. of integers\n");
+    scanf("%d",&n);
+    printf("Enter the elements\n");
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+printf("Enter the no. to be searched\n");
+scanf("%d",&m);
+for(int i=0;i<n;i++)
+{if(a[i]==m)
+{printf("Found %d in position %d\n",m,i+1);
+j=0;
+break;}
+else j=1;
+
+}
+if(j==1)
+printf("not found\n");
+}

2d0b2a727025688c16dfcfa083405cb9ef59c559
aloysiousbenoy <aloysiousbenoy@gmail.com>
2020-06-13 23:14:20 +0530

    Fixed missing bracket for Bsort function

--- bsort.c
+++ bsort.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 void bsort(int a[], int n)
 {
     for (int i = 0; i < n; i++)
-        {for (int j = 0; j < n-i; j++)
+    {
+        for (int j = 0; j < n - i; j++)
         {
             if (a[j + 1] < a[j])
-            {a[j+1]+=a[j];
-            a[j]=a[j+1]-a[j];
-            a[j+1]=a[j+1]-a[j];//swap without using third variable
+            {
+                a[j + 1] += a[j];
+                a[j] = a[j + 1] - a[j];
+                a[j + 1] = a[j + 1] - a[j]; //swap without using third variable
             }
         }
-
+    }
 }
 void main()
 {
@@ -23,7 +25,7 @@ void main()
     printf("Enter the elements\n");
     for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
-    bsort(a,n);
-    for(int i=0;i<n;i++)
-    printf("%d ",a[i]);    
+    bsort(a, n);
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
 }

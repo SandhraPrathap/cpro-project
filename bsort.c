9e0486b3c30b3dd8db25acd5a4c7fc34251ae074
SandhraPrathap <sandhraprathap@gmail.com>
2020-06-13 23:08:40 +0530

    bsort

---
+++ bsort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+void bsort(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        {for (int j = 0; j < n-i; j++)
+        {
+            if (a[j + 1] < a[j])
+            {a[j+1]+=a[j];
+            a[j]=a[j+1]-a[j];
+            a[j+1]=a[j+1]-a[j];//swap without using third variable
+            }
+        }
+
+}
+void main()
+{
+    int n;
+    int *a;
+    printf("Enter the no. of elements\n");
+    scanf("%d", &n);
+    a = (int *)malloc(sizeof(int) * n);
+    printf("Enter the elements\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    bsort(a,n);
+    for(int i=0;i<n;i++)
+    printf("%d ",a[i]);    
+}

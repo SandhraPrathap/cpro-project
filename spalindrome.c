a2e6ec14bf4cd903584a7d0b7b893778b59c48d4
SandhraPrathap <sandhraprathap@gmail.com>
2020-06-14 00:13:32 +0530

    Implemented a c program to check whether input string is palindrome

---
+++ spalindrome.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+    char s[50];
+    int n, j = 0;
+    printf("Enter the string\n");
+    scanf("%[^\n]%*c", s);
+    for (n = 0; s[n] != '\0'; n++)
+        ;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] != s[n - i - 1])
+        {
+            printf("Not Palindrome\n");
+            break;
+            j = 0;
+        }
+        else
+        {
+            j = 1;
+        }
+    }
+    if (j == 1)
+        printf("Palindrome\n");
+}

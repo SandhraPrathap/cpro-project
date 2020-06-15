17c735242f6551de5ff4c2f416337ef8ec11f41a
SandhraPrathap <sandhraprathap@gmail.com>
2020-06-15 23:46:08 +0530

    count vowels,consonants and spaces in a string

---
+++ count.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+    char a[50];
+    int j = 0, k = 0, l = 0;
+    printf("Enter the string\n");
+    scanf("%[^\n]%*c", a);
+    for (int i = 0; a[i] != '$'; i++)
+    {
+        if (a[i] == 'a' || a[i] == 'A' || a[i] == 'e' || a[i] == 'E' || a[i] == 'i' || a[i] == 'I' || a[i] == 'o' || a[i] == 'O' || a[i] == 'u' || a[i] == 'U')
+            j++;
+        else if (a[i] == ' ')
+            k++;
+        else
+        {
+            l++;
+        }
+    }
+    printf("Vowels: %d\n", j);
+    printf("Consonants:%d\n", l);
+    printf("space:%d\n", k);
+}
